cbefcfc7ebff7f5c7bfcefe5a3d183fc69c12b2c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-11-08 13:12:57 +0000

    Some small cleanups.

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'latexcodec'
-copyright = u'2011, Matthias C. M. Troffaes'
+copyright = u'2011-2013, Matthias C. M. Troffaes'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -123,7 +123,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
