73f8b5a17a8f707e1e8e0e000c1dbd380ce83090
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-06-12 09:06:30 +0100

    sphinx.ext.pngmath -> sphinx.ext.imgmath

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinx.ext.viewcode']
 source_suffix = '.rst'
 master_doc = 'index'
