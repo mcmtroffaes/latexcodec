6e9b5d5af8e25825faa30f44cd05029acec01a5a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-01-18 13:20:58 +0000

    Fix backslash.

--- latexcodec/codec.py
+++ latexcodec/codec.py
@@ -103,7 +103,7 @@ class LatexUnicodeTable:
         self.register(u'\n\n', b' \\par', encode=False)
         self.register(u'\n\n', b'\\par', encode=False)
         self.register(u' ', b'\\ ', encode=False)
-        self.register(u'\N{EM SPACE}', b'\quad')
+        self.register(u'\N{EM SPACE}', b'\\quad')
         self.register(u'%', b'\\%')
         self.register(u'\N{EN DASH}', b'--')
         self.register(u'\N{EN DASH}', b'\\textendash')
