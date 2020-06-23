fbae3391eee8214ccf44a63cc84bc211c5168a4f
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-06-23 10:55:18 +0100

    Flake fix.

--- latexcodec/codec.py
+++ latexcodec/codec.py
@@ -125,7 +125,8 @@ class LatexUnicodeTable:
         self.register(u'\N{MODIFIER LETTER DOUBLE PRIME}', u"''", decode=False)
         self.register(u'\N{MODIFIER LETTER TURNED COMMA}', u'`', decode=False)
         self.register(u'\N{MODIFIER LETTER APOSTROPHE}', u"'", decode=False)
-        self.register(u'\N{MODIFIER LETTER REVERSED COMMA}', u'`', decode=False)
+        self.register(u'\N{MODIFIER LETTER REVERSED COMMA}', u'`',
+                      decode=False)
         self.register(u'\N{DAGGER}', u'\\dag')
         self.register(u'\N{DOUBLE DAGGER}', u'\\ddag')
 
